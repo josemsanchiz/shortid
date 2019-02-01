aac27c58587309a632ce6c65b8b0e791f81e4be6
Jose Miguel Sanchiz <jose-miguel.sanchiz@vtlab.net>
2019-02-01 12:54:17 +0100

    Added config obejcts and methods

--- examples/examples.js
+++ examples/examples.js
@@ -1,8 +1,15 @@
 var shortId = require('..');
 
+shortId.config({
+    disableDefaultAlphabetLength: true,
+    disableDefaultIdLength: true,
+    idLength: 6
+})
+shortId.characters('123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+
 //Generate 10 ids
 
-var i = 10;
+var i = 25000;
 while (i--) {
     console.log(shortId.generate());
 }
